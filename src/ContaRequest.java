6fd2aeb79cd92a1bf824c663e6897c085855050b
uaianaluiza <p0661188@portoseguro.com.br>
2024-11-09 09:16:57 -0300

    Atualiza a request para sempre criar a conta com o saldo zerado

--- src/ContaRequest.java
+++ src/ContaRequest.java
@@ -1,15 +1,14 @@
 public class ContaRequest {
     int agencia;
     int numero;
-    double saldo;
-    TipoConta tipoConta;
+    double saldo = 0;
     Cliente cliente;
+    int tipoConta;
 
-    public ContaRequest(int agencia, int numero, double saldo, TipoConta tipoConta, Cliente cliente) {
+    public ContaRequest(int agencia, int numero, Cliente cliente, int tipoConta) {
         this.agencia = agencia;
         this.numero = numero;
-        this.saldo = saldo;
-        this.tipoConta = tipoConta;
         this.cliente = cliente;
+        this.tipoConta = tipoConta;
     }
 }
